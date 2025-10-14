458bcb4b613ff4f03e14ed5482d6b871c6fba24e
Daniel <108305782+dvelkakov@users.noreply.github.com>
2025-10-14 23:26:47 +0100

    Deploy

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9j9!+p17_aphxdso_ijz+8s=#&zw&kjdf4$74v1nxn#cwiisja'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 1)))
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
+
+STATIC_ROOT = "static/"
